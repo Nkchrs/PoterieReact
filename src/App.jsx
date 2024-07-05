cdcaad286c5bfb27c24ca45fa965bde3f191f74d
Nkchrs <nkchrs@hotmail.fr>
2024-07-05 12:25:46 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "./components/Navar.jsx/navbar.jsx";
+
+const App = () => { 
+  return ( 
+    <div className="overflow-x-hidden">
+      <Navbar />
+    </div>
+  );
+  
+    
+}
+
+export default App
---
+++ src/components/Navar.jsx/navbar.jsx
@@ -0,0 +1,106 @@
+import { data } from "autoprefixer";
+import React from "react";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+
+const NavLinks = [
+{
+    id: 1,
+    name: "Home" ,
+    link: "/#",
+},
+{
+    id: 1,
+    name: "About" ,
+    link: "/#",
+},
+{
+    id: 1,
+    name: "Contact" ,
+    link: "/#",
+}
+];
+
+const CategorieLinks = [
+    {
+        id: 1,
+        name: "faïence" ,
+        link: "/#",
+    },
+    {
+        id: 2,
+        name: "Porcelaine" ,
+        link: "/#",
+    },
+    {
+        id: 3,
+        name: "Grès" ,
+        link: "/#",
+    }
+];
+
+const Navbar = () =>{
+    return (
+        <>
+        <div className="bg-white shadow-sm">
+            <div className="container flex justify-between py-4 sm:py-3">
+                {}
+                <div className="font-bold text-3xl">Mel'in Pots</div>
+                {}
+                <div>
+                <ul className="flex items-center gap-10">
+                    {NavLinks.map(({id, name, link})=>(
+                        <li key={id}>
+                            <a href={link}
+                            className="inline-block hover:text-primaire text-xl font-semibold "
+                            >{name}</a>
+                        </li>
+                    ))}
+                    {}
+                    <li className="cursor-pointer group">
+                         <a
+                        href="/#"
+                        className="inline-block hover:text-primaire text-xl font-semibold"
+                        >
+                             <div className="flex items-center gap-[2px] py-2">
+                           Categorie
+                                <span>
+                            <IoIosArrowDropdownCircle className="group-hover:rotate-180 duration-300" />
+                                </span>
+                            </div>
+                        </a>
+                        {}
+                        <div className="absolute z-[9999] hidden
+                        group-hover:block w-[200px] bg-white text-black shadow-md">
+                            <ul>
+                            { CategorieLinks.map(({id,name,link})=> (
+                                <li key={id}>
+                                    <a
+                                    href={link}
+                                    className="inline-block
+                                     hover:bg-primaire 
+                                     text-xl font-semibol
+                                     rounded-md p-2
+                                     text-cursive"
+                                     
+                                    >
+                                     {name}
+                                    </a>
+                                </li>
+                                 ) )
+                                }
+
+                            </ul>
+                        </div>
+                    </li>
+                 </ul>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+
+
+
+export default Navbar
